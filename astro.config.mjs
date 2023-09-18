b51b7d14951e239352c6d174fa8397f67e69a575
Reupen Shah <reupen@users.noreply.github.com>
2023-09-18 23:05:55 +0100

    Temp

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,11 @@ export default defineConfig({
   scopedStyleStrategy: "class",
   integrations: [
     compress({
-      css: false,
-      html: true,
-      img: false,
-      js: false,
-      svg: true,
+      CSS: false,
+      HTML: true,
+      Image: false,
+      JavaScript: false,
+      SVG: true,
     }),
   ],
   // vite: {
