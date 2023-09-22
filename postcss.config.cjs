c8367157993197af157f3d6df63fb4a37c0a6d54
Reupen Shah <reupen@users.noreply.github.com>
2023-09-22 20:41:06 +0100

    Temp

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,10 @@
 const cssnano = require("cssnano")
 const postcssPresetEnv = require("postcss-preset-env")
+const postcssCustomMedia = require("postcss-custom-media");
 
 module.exports = {
   plugins: [
+    postcssCustomMedia(),
     postcssPresetEnv({ stage: 1 }),
     cssnano({
       preset: "default",
