88a040a0a01cc120a5cfa10dcff77b4cd7808291
Reupen Shah <reupen@users.noreply.github.com>
2023-09-23 16:49:53 +0100

    Temp

--- scripts/run_e2e_tests.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-from utils.container import run_in_playwright_container
-
-if __name__ == "__main__":
-    run_in_playwright_container("npm ci && npm exec --no -- playwright test")
