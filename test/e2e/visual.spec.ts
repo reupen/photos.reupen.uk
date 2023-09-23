c91704fa58cf8657c32ed468169b0598df09969d
Reupen Shah <reupen@users.noreply.github.com>
2023-09-23 15:44:18 +0100

    Temp

--- test/e2e/visual.spec.ts
+++ test/e2e/visual.spec.ts
@@ -8,7 +8,7 @@ PAGES.forEach((path) => {
       await page.goto(path)
       await expect(page).toHaveScreenshot(
         [path.replaceAll(/(^\/|\/$)/g, ""), "screenshot.png"],
-        { fullPage: true },
+        { fullPage: true, timeout: 10_000 },
       )
     })
   })
