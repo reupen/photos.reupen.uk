b86787c115baa5ef7949b046fa169374c14ce877
Reupen Shah <reupen@users.noreply.github.com>
2024-11-10 19:33:12 +0000

    Update dependencies

--- test/e2e/visual.spec.ts
+++ test/e2e/visual.spec.ts
@@ -6,7 +6,9 @@ PAGES.forEach(({ path, takeViewportScreenshot }) => {
     test("matches the saved screenshot", async ({ page }) => {
       await page.goto(path)
       await expect(page).toHaveScreenshot(
-        [path.replaceAll(/(^\/|\/$)/g, ""), "screenshot.png"],
+        [path.replaceAll(/(^\/|\/$)/g, ""), "screenshot.png"].filter(
+          (pathSegment) => pathSegment,
+        ),
         {
           fullPage: !takeViewportScreenshot,
           timeout: process.env.CI ? 40_000 : 15_000,
